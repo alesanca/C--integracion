2615c14deef6b91e7e4561fbb560298bfdcd05be
alesanca <alesan.ca3@gmail.com>
2020-03-12 10:34:59 +0000

    Primer commit del proyecto de integracion

---
+++ integracion2.0/sites/default/files/php/twig/5e68e82cbbb13_page.html.twig_ILyg0z3ChNqLBHXabtQmz3lzq/bdcOysY1LqDZhjvjtWT3Kgotzqx5vQ4tB6CYPhsDsJY.php
@@ -0,0 +1,247 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/themes/bartik/templates/page.html.twig */
+class __TwigTemplate_ad7a3fce20a49c653d5dcce4a36db37d2d7553dac14d8b1ac29b9a634dc37326 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["if" => 11];
+        $filters = ["escape" => 6];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 1
+        echo "
+<div id=\"page-wrapper\">
+  <div id=\"page\">
+    <header id=\"header\" class=\"header\" role=\"banner\">
+      <div class=\"section layout-container clearfix\">
+        ";
+        // line 6
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "secondary_menu", [])), "html", null, true);
+        echo "
+        ";
+        // line 7
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "header", [])), "html", null, true);
+        echo "
+        ";
+        // line 8
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "primary_menu", [])), "html", null, true);
+        echo "
+      </div>
+    </header>
+    ";
+        // line 11
+        if ($this->getAttribute(($context["page"] ?? null), "highlighted", [])) {
+            // line 12
+            echo "      <div class=\"highlighted\">
+        <aside class=\"layout-container section clearfix\" role=\"complementary\">
+          ";
+            // line 14
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "highlighted", [])), "html", null, true);
+            echo "
+        </aside>
+      </div>
+    ";
+        }
+        // line 18
+        echo "    ";
+        if ($this->getAttribute(($context["page"] ?? null), "featured_top", [])) {
+            // line 19
+            echo "      <div class=\"featured-top\">
+        <aside class=\"featured-top__inner section layout-container clearfix\" role=\"complementary\">
+          ";
+            // line 21
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "featured_top", [])), "html", null, true);
+            echo "
+        </aside>
+      </div>
+    ";
+        }
+        // line 25
+        echo "    <div id=\"main-wrapper\" class=\"layout-main-wrapper layout-container clearfix\">
+      <div id=\"main\" class=\"layout-main clearfix\">
+        ";
+        // line 27
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "breadcrumb", [])), "html", null, true);
+        echo "
+        <main id=\"content\" class=\"column main-content\" role=\"main\">
+          
+          <section class=\"section\">
+            <a id=\"main-content\" tabindex=\"-1\"></a>
+            ";
+        // line 32
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "content", [])), "html", null, true);
+        echo "
+          </section>
+          
+        </main>
+        ";
+        // line 36
+        if ($this->getAttribute(($context["page"] ?? null), "sidebar_first", [])) {
+            // line 37
+            echo "          <div id=\"sidebar-first\" class=\"column sidebar\">
+            <aside class=\"section\" role=\"complementary\">
+              ";
+            // line 39
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "sidebar_first", [])), "html", null, true);
+            echo "
+            </aside>
+          </div>
+        ";
+        }
+        // line 43
+        echo "        ";
+        if ($this->getAttribute(($context["page"] ?? null), "sidebar_second", [])) {
+            // line 44
+            echo "          <div id=\"sidebar-second\" class=\"column sidebar\">
+            <aside class=\"section\" role=\"complementary\">
+              ";
+            // line 46
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "sidebar_second", [])), "html", null, true);
+            echo "
+            </aside>
+          </div>
+        ";
+        }
+        // line 50
+        echo "      </div>
+    </div>
+    ";
+        // line 52
+        if ((($this->getAttribute(($context["page"] ?? null), "featured_bottom_first", []) || $this->getAttribute(($context["page"] ?? null), "featured_bottom_second", [])) || $this->getAttribute(($context["page"] ?? null), "featured_bottom_third", []))) {
+            // line 53
+            echo "      <div class=\"featured-bottom\">
+        <aside class=\"layout-container clearfix\" role=\"complementary\">
+          ";
+            // line 55
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "featured_bottom_first", [])), "html", null, true);
+            echo "
+          ";
+            // line 56
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "featured_bottom_second", [])), "html", null, true);
+            echo "
+          ";
+            // line 57
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "featured_bottom_third", [])), "html", null, true);
+            echo "
+        </aside>
+      </div>
+    ";
+        }
+        // line 61
+        echo "    <footer class=\"site-footer\">
+      <div class=\"layout-container\">
+        ";
+        // line 63
+        if (((($this->getAttribute(($context["page"] ?? null), "footer_first", []) || $this->getAttribute(($context["page"] ?? null), "footer_second", [])) || $this->getAttribute(($context["page"] ?? null), "footer_third", [])) || $this->getAttribute(($context["page"] ?? null), "footer_fourth", []))) {
+            // line 64
+            echo "          <div class=\"site-footer__top clearfix\">
+            ";
+            // line 65
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "footer_first", [])), "html", null, true);
+            echo "
+            ";
+            // line 66
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "footer_second", [])), "html", null, true);
+            echo "
+            ";
+            // line 67
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "footer_third", [])), "html", null, true);
+            echo "
+            ";
+            // line 68
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "footer_fourth", [])), "html", null, true);
+            echo "
+          </div>
+        ";
+        }
+        // line 71
+        echo "        ";
+        if ($this->getAttribute(($context["page"] ?? null), "footer_fifth", [])) {
+            // line 72
+            echo "          <div class=\"site-footer__bottom\">
+            ";
+            // line 73
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "footer_fifth", [])), "html", null, true);
+            echo "
+          </div>
+        ";
+        }
+        // line 76
+        echo "      </div>
+    </footer>
+  </div>
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/bartik/templates/page.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  213 => 76,  207 => 73,  204 => 72,  201 => 71,  195 => 68,  191 => 67,  187 => 66,  183 => 65,  180 => 64,  178 => 63,  174 => 61,  167 => 57,  163 => 56,  159 => 55,  155 => 53,  153 => 52,  149 => 50,  142 => 46,  138 => 44,  135 => 43,  128 => 39,  124 => 37,  122 => 36,  115 => 32,  107 => 27,  103 => 25,  96 => 21,  92 => 19,  89 => 18,  82 => 14,  78 => 12,  76 => 11,  70 => 8,  66 => 7,  62 => 6,  55 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "core/themes/bartik/templates/page.html.twig", "C:\\xampp_drupal\\htdocs\\integracion2.0\\core\\themes\\bartik\\templates\\page.html.twig");
+    }
+}
---
+++ integracion2.0/sites/default/files/php/twig/5e68e82cbbb13_superfish.html.twig_o95WmkOBdWmBJ6pfdZay0rByk/jFVElCZKEnjtbW2D65y2vgrV0eiT8oS0WHnLmJafcsI.php
@@ -0,0 +1,97 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/superfish/templates/superfish.html.twig */
+class __TwigTemplate_8ed99ea53cd29e810308aca862e3fc67faab2ea115d04f3bb4a6a3b709d10fd7 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = [];
+        $filters = ["escape" => 17];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 16
+        echo "
+<ul id=\"";
+        // line 17
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["id"] ?? null)), "html", null, true);
+        echo "\" class=\"";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["menu_classes"] ?? null)), "html", null, true);
+        echo "\">
+  ";
+        // line 18
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["menu_items"] ?? null)), "html", null, true);
+        echo "
+</ul>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/superfish/templates/superfish.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  64 => 18,  58 => 17,  55 => 16,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "modules/superfish/templates/superfish.html.twig", "C:\\xampp_drupal\\htdocs\\integracion2.0\\modules\\superfish\\templates\\superfish.html.twig");
+    }
+}
